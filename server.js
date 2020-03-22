9b6ec61a8b98118287126c0f939886175cc5068c
Ankit <ankit.gupta@gmail.com>
2020-03-21 23:18:08 -0400

    NLP integrated

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ const app = express()
 // )
 
 // FOR LOCAL ENV
-// const API_KEY = process.env.GOOGLE_APPLICATION_CREDENTIALS
+const API_KEY = process.env.GOOGLE_APPLICATION_CREDENTIALS
 
 // FOR HEROKU ENV
-const GOOGLE_APPLICATION_CREDENTIALS = JSON.parse(
-	process.env.GOOGLE_APPLICATION_CREDENTIALS
-)
+// const GOOGLE_APPLICATION_CREDENTIALS = JSON.parse(
+// 	process.env.GOOGLE_APPLICATION_CREDENTIALS
+// )
 
 // console.log('++++++++++++++++ PRINTING API KEY +++++++++++')
 // console.log(API_KEY)
@@ -30,6 +30,7 @@ app.use(cors())
 
 app.use(express.static('./'))
 
+// ================================ SPEECH
 async function getSpeechToText(audioBuffer) {
 	const speech = require('@google-cloud/speech')
 	const client = new speech.SpeechClient({
@@ -58,7 +59,7 @@ async function getSpeechToText(audioBuffer) {
 
 // HOME
 app.get('/', (req, res) => {
-	res.send('Server Js')
+	res.send('Speech Server Js')
 })
 
 // GET AUDIO FILE FROM CLIENT
@@ -67,8 +68,60 @@ app.post('/upload_sound', upload.any(), async (req, res) => {
 	let transcription = await getSpeechToText(req.files[0].buffer).catch(
 		console.error
 	)
+	let things = await understandSyntax(transcription).catch(console.error)
 	console.log('Text transcription: ' + transcription)
-	res.status(200).send(transcription)
+	let result = {
+		transcript: transcription,
+		things: things
+	}
+	res.status(200).send(result)
+})
+
+// ======================= NATURAL LANGUAGE API
+// let textData = {
+// 	document: {
+// 		type: 'PLAIN_TEXT',
+// 		content:
+// 			'Joanne Rowling, who writes under the pen names J. K. Rowling and Robert Galbraith, is a British novelist and screenwriter who wrote the Harry Potter fantasy series.'
+// 	},
+// 	encodingType: 'UTF8'
+// }
+//
+async function understandSyntax(content) {
+	// Imports the Google Cloud client library
+	const language = require('@google-cloud/language')
+
+	// Instantiates a client
+	const client = new language.LanguageServiceClient()
+
+	// The text to analyze
+	const text = 'Draw a butterfly and a duck.'
+
+	const document = {
+		content: content,
+		type: 'PLAIN_TEXT'
+	}
+
+	// Need to specify an encodingType to receive word offsets
+	const encodingType = 'UTF8'
+
+	// Detects the sentiment of the text
+	const [result] = await client.analyzeSyntax({ document, encodingType })
+
+	// COLLECT NOUNS IN AN ARRAY
+	let things = []
+	if (result.tokens) {
+		result.tokens.forEach(poS => {
+			if (poS.partOfSpeech.tag == 'NOUN') things.push(poS.text.content)
+		})
+	}
+
+	return things
+}
+//
+app.get('/language', async (req, res) => {
+	let nlpResult = await understandSyntax().catch(console.error)
+	res.status(200).send(nlpResult)
 })
 
 //
