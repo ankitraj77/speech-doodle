12a69ac09fe6b0e730c27072c7df0b356275ee59
Ankit <ankit.gupta@gmail.com>
2020-04-05 17:42:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ async function main() {
 	const audioBytes = file.toString('base64')
 
 	const audio = {
-		content: audioBytes
+		content: audioBytes,
 	}
 
 	const config = {
 		encoding: 'LINEAR16',
-		sampleRateHertz: 44100,
-		languageCode: 'en-US'
+		// sampleRateHertz: 44100,
+		languageCode: 'en-US',
 	}
 
 	const request = {
 		audio: audio,
-		config: config
+		config: config,
 	}
 
 	const [response] = await client.recognize(request)
 	const transcription = response.results
-		.map(result => result.alternatives[0].transcript)
+		.map((result) => result.alternatives[0].transcript)
 		.join('\n')
 	console.log(`Transcription: ${transcription}`)
 }
